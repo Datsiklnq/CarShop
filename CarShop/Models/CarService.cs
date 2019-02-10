91e6dfdfb43779c36f668b90636af98b3e32ba97
Przemek Kieloch <datsiklnq@gmail.com>
2019-02-10 14:52:17 +0000

    Added CarService class and ICarRepository Interface

---
+++ CarShop/Models/CarService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarShop.Models
+{
+    public class CarService : ICarRepository
+    {
+        private readonly CarShopContext _context;
+
+        public CarService(CarShopContext context)
+        {
+            _context = context;
+        }
+
+        public void AddNewCar(Car car)
+        {
+            _context.Add(car);
+            _context.SaveChanges();
+        }
+
+        public List<Car> GetCar()
+        {
+            return _context.Car.ToList();
+        }
+
+        public Car GetCarById(int? id)
+        {
+            var car = _context.Car
+                .FirstOrDefault(p => p.Id == id);
+
+            return car;
+        }
+
+        public void RemoveCar(Car car)
+        {
+            _context.Car.Remove(car);
+            _context.SaveChanges();
+        }
+
+        public void UpdateCar(Car car)
+        {
+            _context.Update(car);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ CarShop/Models/ICarRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarShop.Models
+{
+   public interface ICarRepository
+    {
+
+        List<Car> GetCar();
+        Car GetCarById(int? id);
+        void AddNewCar(Car car);
+        void UpdateCar(Car car);
+        void RemoveCar(Car car);
+    }
+}
